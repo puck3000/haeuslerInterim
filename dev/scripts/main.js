e4e46c7cf9517283a2239a7bb1f3b12446388ef6
puck3000 <daniel.lerch@gmail.com>
2015-11-22 10:11:16 +0100

    ready for first deploy of second version

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,14 @@
 $(function() {  
-   console.log('hello world!');
+   $('.main-gallery').flickity({
+   		contain: true,
+   		imagesLoaded: true,
+   		prevNextButtons: false,
+		pageDots: false,
+		wrapAround: true,
+		autoPlay: 4000,
+		draggable: false,
+		lazyLoad: true,
+		// setGallerySize: false,
+		cellAlign: 'center'
+	});
 });
